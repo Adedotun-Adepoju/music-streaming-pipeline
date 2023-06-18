fbf1cb110e30abdf0f94d92805df96c44b40c807
Adedotun Adepoju <d.e.adepoju@gmail.com>
2023-06-18 16:17:36 +0100

    refactor spark code

--- eventsim_producer.sh
+++ eventsim_producer.sh
@@ -6,6 +6,7 @@ echo "Building docker image to generate events...."
 docker build -t eventsim .
 
 echo "Running the container to generate events..."
+docker rm streaming_events
 docker run -itd \
     --network host \
     --name streaming_events \
@@ -14,7 +15,7 @@ docker run -itd \
     eventsim \
         -c "examples/example-config.json" \
         --start-time "2015-06-01T00:00:00" \
-        --end-time "2016-02-01T00:00:00" \
+        --end-time "2015-06-02T00:00:00" \
         --nusers 5000 \
         --growth-rate 0.25 \
         --userid 1 \
