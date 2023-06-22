c0d891ffe75a0a5a5c27a599291095dc9a8c7d97
Adedotun Adepoju <d.e.adepoju@gmail.com>
2023-06-22 19:33:38 +0100

    Add resoure to create dataproc cluster

--- terraform/main.tf
+++ terraform/main.tf
@@ -14,3 +14,34 @@ provider "google" {
 }
 
 # Dataproc
+resource "google_dataproc_cluster" "music-streaming-cluster"{
+	name = "music-streaming-cluster"
+	region = var.region
+	zone = var.zone
+
+	cluster_config {
+		staging_bucket = "dataproc-staging-bucket"
+
+		software_config {
+			override_properties = {
+				"dataproc:dataproc.allow.zero.workers" = "true"
+			}
+
+			optional_components = ["DOCKER", "JUPYTER"]
+		}
+
+		master_config{
+      num_instances = 1
+      machine_type  = "n1-standard-4"
+
+      disk_config {
+        boot_disk_type    = "pd-standard"
+        boot_disk_size_gb = 500
+      }
+		}
+
+		worker_config {
+			num_instances = 0
+		}
+	}
+}
