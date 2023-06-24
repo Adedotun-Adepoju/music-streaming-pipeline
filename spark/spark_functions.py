4b0e13365734dfa88f0bbd29055f7e00f7e3b833
Adedotun Adepoju <d.e.adepoju@gmail.com>
2023-06-24 14:31:42 +0000

    add functuion to get last line of a file from gcs

--- spark/spark_functions.py
+++ spark/spark_functions.py
@@ -1,6 +1,35 @@
 from pyspark.sql.functions import from_json, col
+import subprocess
+
+def get_last_offsets(checkpointLocation):
+    """
+    
+    """
+    # Bash command to get last file in the GCS bucket for storing offsets
+    # bash_command = f"gsutil ls -lh gs://music_streams_spark_jobs/checkpoints/offsets/ | sort -k 2 | tail -n 1"
+
+    bash_command = f"gsutil ls -lh {checkpointLocation}/offsets/ | sort -k 2 | tail -n 1"
+
+    process = subprocess.Popen(bash_command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    output, error = process.communicate()
+
+    # Print the output and errror (if any)
+    print("Command output:", output.decode())
+    print("Command error:", error.decode())
+
+    file_name = output.split()[-1].decode()
+    print("file_name", file_name)
+
+    last_line_command = f"gsutil cat {checkpointLocation}/offsets/1 | tail -n 1"
+    print(last_line_command)
+    last_line_process = subprocess.Popen(last_line_command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+    output, error = last_line_process.communicate()
+
+    return output.decode()
 
 def process_events(spark, kafka_server, topic, schema, starting_offset="latest"):
+    print("here", starting_offset)
     """
     Process specified events from kafka topics
 
@@ -19,7 +48,7 @@ def process_events(spark, kafka_server, topic, schema, starting_offset="latest")
         spark.readStream.format("kafka")
         .option("kafka.bootstrap.servers", kafka_server)
         .option("subscribe", topic)
-        .option("startingOffsets", starting_offset)
+        .option("startingOffsets", latest)
         .load()
         .selectExpr("CAST(value AS STRING)")
         .select(from_json(col("value"), schema).alias("data"))
--- spark/spark_script.py
+++ spark/spark_script.py
@@ -2,13 +2,10 @@ from pyspark import SparkContext, SparkConf
 from pyspark.sql import SparkSession
 from pyspark.sql.types import StructField, StructType, IntegerType, StringType, FloatType
 from pyspark.sql.functions import from_json, col
-from spark_functions import process_events
+from spark_functions import process_events, get_last_offsets
 from schemas import listen_events_schema, auth_events_schema, page_view_events_schema, status_change_events_schema
 import time
-from kafka import KafkaConsumer, TopicPartition
-# spark = SparkSession.builder.appName("read_test_stream").getOrCreate()
 
-# spark.sparkContext.setLogLevel("ERROR")
 spark = (SparkSession
     .builder
     .appName("Eventsim Stream")
@@ -23,25 +20,20 @@ PAGE_VIEW_EVENTS_TOPIC = "page_view_events"
 STATUS_CHANGE_EVENTS = "status_change_events"
 KAFKA_BOOTSTRAP_SERVER = "34.79.233.22:9092"
 
-GCS_STORAGE_PATH = "gs://music_streams_spark_jobs/files"
-GCS_CHECKPOINT_PATH = "gs://music_streams_spark_jobs/checkpoints"
+GCS_BUCKET = "music_streams_spark_jobs"
 
-df_listen_events = process_events(spark, KAFKA_BOOTSTRAP_SERVER, LISTEN_EVENTS_TOPIC, listen_events_schema)
-df_auth_events = process_events(spark, KAFKA_BOOTSTRAP_SERVER, AUTH_EVENTS_TOPIC, auth_events_schema)
-df_page_view_events = process_events(spark, KAFKA_BOOTSTRAP_SERVER, PAGE_VIEW_EVENTS_TOPIC, page_view_events_schema)
-df_status_events = process_events(spark, KAFKA_BOOTSTRAP_SERVER, STATUS_CHANGE_EVENTS, status_change_events_schema)
+GCS_STORAGE_PATH = f"gs://{GCS_BUCKET}/files"
+GCS_CHECKPOINT_PATH = f"gs://{GCS_BUCKET}/checkpoints"
+
+offset = get_last_offsets(GCS_CHECKPOINT_PATH)
+print(offset)
+
+df_listen_events = process_events(spark, KAFKA_BOOTSTRAP_SERVER, LISTEN_EVENTS_TOPIC, listen_events_schema, offset)
+# df_auth_events = process_events(spark, KAFKA_BOOTSTRAP_SERVER, AUTH_EVENTS_TOPIC, auth_events_schema)
+# df_page_view_events = process_events(spark, KAFKA_BOOTSTRAP_SERVER, PAGE_VIEW_EVENTS_TOPIC, page_view_events_schema)
+# df_status_events = process_events(spark, KAFKA_BOOTSTRAP_SERVER, STATUS_CHANGE_EVENTS, status_change_events_schema)
 
 df_listen_events.printSchema()
-
-# Convert binary value to string. The value is the data we are actually interested in
-# deserialized_df = df_listen_events.selectExpr("CAST(value AS STRING)")\
-#     .select(from_json(col("value"), listen_events_schema).alias("data"))\
-#         .select("data.*")
-
-# query = df_listen_events.writeStream \
-#     .format("console") \
-#     .outputMode("append") \
-#     .start() 
 
 write_stream_writer = (df_listen_events
     .writeStream
@@ -49,10 +41,10 @@ write_stream_writer = (df_listen_events
     # .partitionBy("month", "day", "hour")
     .option("path", GCS_STORAGE_PATH)
     .option("checkpointLocation", GCS_CHECKPOINT_PATH)
-    # .trigger(processingTime="120 seconds")
+    .trigger(processingTime="300 seconds")
     .outputMode("append")
 )
 
 write_stream_writer.start()
 
-# spark.streams.awaitAnyTermination()
+spark.streams.awaitAnyTermination()
