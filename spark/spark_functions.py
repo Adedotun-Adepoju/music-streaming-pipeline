cc649dd3557497091a9e37310d1bd896bc7ce825
Adedotun Adepoju <d.e.adepoju@gmail.com>
2023-07-02 21:30:06 +0100

    modify function

--- spark/spark_functions.py
+++ spark/spark_functions.py
@@ -1,32 +1,33 @@
 from pyspark.sql.functions import from_json, year, month, dayofmonth, hour
 import subprocess
 
-# def get_last_offsets(checkpointLocation):
-#     """
+def get_last_offsets(checkpointLocation):
+    """
     
-#     """
-#     # Bash command to get last file in the GCS bucket for storing offsets
-#     # bash_command = f"gsutil ls -lh gs://music_streams_spark_jobs/checkpoints/offsets/ | sort -k 2 | tail -n 1"
+    """
+    # Bash command to get last file in the GCS bucket for storing offsets
+    # bash_command = f"gsutil ls -lh gs://music_streams_spark_jobs/checkpoints/offsets/ | sort -k 2 | tail -n 1"
 
-#     bash_command = f"gsutil ls -lh {checkpointLocation}/offsets/ | sort -k 2 | tail -n 1"
+    # Command to read the last line of the most recent file using the timestamp
+    bash_command = f"gsutil ls -lh {checkpointLocation}/offsets/ | sort -k 2 | tail -n 1"
 
-#     process = subprocess.Popen(bash_command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-#     output, error = process.communicate()
+    process = subprocess.Popen(bash_command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    output, error = process.communicate()
 
-#     # Print the output and errror (if any)
-#     print("Command output:", output.decode())
-#     print("Command error:", error.decode())
+    # Print the output and errror (if any)
+    print("Command output:", output.decode())
+    print("Command error:", error.decode())
 
-#     file_name = output.split()[-1].decode()
-#     print("file_name", file_name)
+    file_name = output.split()[-1].decode()
+    print("file_name", file_name)
 
-#     last_line_command = f"gsutil cat {checkpointLocation}/offsets/1 | tail -n 1"
-#     print(last_line_command)
-#     last_line_process = subprocess.Popen(last_line_command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    last_line_command = f"gsutil cat {checkpointLocation}/offsets/{ file_name } | tail -n 1"
+    print(last_line_command)
+    last_line_process = subprocess.Popen(last_line_command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
-#     output, error = last_line_process.communicate()
+    output, error = last_line_process.communicate()
 
-#     return output.decode()
+    return output.decode()
 
 def process_events(spark, kafka_server, topic, schema, starting_offset="latest"):
     """
@@ -43,21 +44,22 @@ def process_events(spark, kafka_server, topic, schema, starting_offset="latest")
         Processed Dataframe
     """
 
-    kafka_stream = (
-        spark.readStream.format("kafka")
+    df = (
+        spark.read.format("kafka")
         .option("kafka.bootstrap.servers", kafka_server)
         .option("subscribe", topic)
         .option("startingOffsets", starting_offset)
+        .option("endingOffsets", "latest")
         .load()
     )
 
-    spark_df = (kafka_stream       
+    df = (df       
         .selectExpr("CAST(value AS STRING)")
         .select(from_json(col("value"), schema).alias("data"))
         .select("data.*")
     )
 
-    spark_df = (spark_df
+    df = (df
         .withColumn('date', (spark_df['ts']/1000).cast("string"))
         .withColumn('year', year(spark_df["timestamp"]))
         .withColumn('month', month(spark_df["timestamp"]))
@@ -66,9 +68,9 @@ def process_events(spark, kafka_server, topic, schema, starting_offset="latest")
         .withColumn('full_name', concat(spark_df["firstName"], lit(" "), df["lastName"]))
     )
 
-    spark_df = (spark_df
+    df = (df
         .withColumnRenamed("lon", "longitude")
         .withColumnRenamed("lat", "latitude")
     )
 
-    return spark_df
+    return df
--- spark/spark_script.py
+++ spark/spark_script.py
@@ -2,7 +2,7 @@ from pyspark import SparkContext, SparkConf
 from pyspark.sql import SparkSession
 from pyspark.sql.types import StructField, StructType, IntegerType, StringType, FloatType
 from pyspark.sql.functions import from_json, col
-from spark_functions import process_events
+from spark_functions import process_events, get_last_offsets
 from schemas import listen_events_schema, auth_events_schema, page_view_events_schema, status_change_events_schema
 import time
 import datetime
@@ -33,6 +33,10 @@ SPARK_JOBS_BUCKET = "music_streams_spark_jobs"
 
 GCS_STORAGE_PATH = f"gs://{GCS_BUCKET}/files/listen_events/{year}/{month}/{day}/{hour}"
 GCS_CHECKPOINT_PATH = f"gs://{SPARK_JOBS_BUCKET}/tracking"
+
+# Get the latest offsets
+offset = get_last_offsets(GCS_CHECKPOINT_PATH)
+print("offset", offset)
 
 df_listen_events = process_events(spark, KAFKA_BOOTSTRAP_SERVER, LISTEN_EVENTS_TOPIC, listen_events_schema)
 
